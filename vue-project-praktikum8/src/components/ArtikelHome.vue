2851bdca3bd79a7fb4fda83b723a61501f7fb6dd
RifaiRifntoro <rifaii@student.amikom.ac.id>
2024-05-30 17:11:39 +0700

    Prak8

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+    </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArtikelHome from '../components/ArtikelHome.vue'
+
+
+</script>
+
+<template>
+  <main>
+<ArtikelHome>-1 hyper
+  <img src="./assets/logo.svg" alt="">
+</ArtikelHome>
+  </main>
+</template>
